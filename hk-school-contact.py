33028e7db184e12d5cc9d3e1b73d404683e515fd
Ka Chun, Wu <118832447+kachunwu@users.noreply.github.com>
2022-11-28 00:56:35 +0800

    Update hk-school-contact.py

--- hk-school-contact.py
+++ hk-school-contact.py
@@ -3,6 +3,55 @@ import re
 import sqlite3
 from bs4 import BeautifulSoup
 
+def name_str(input):
+    try:
+        str_index = re.search(r"先生", input).start() 
+        output = input[0:str_index+2]
+        return output
+    except AttributeError:    
+        try:
+            str_index = re.search(r"女士", input).start()
+            output = input[0:str_index+2]
+            return output
+        except AttributeError:
+            try:
+                str_index = re.search(r"博士", input).start()
+                output = input[0:str_index+2]
+                return output
+            except AttributeError:
+                try:
+                    str_index = re.search(r"校長", input).start()
+                    output = input[0:str_index+2]
+                    return output
+                except AttributeError:
+                    try:
+                        str_index = re.search(r"太平紳士", input).start()
+                        output = input[0:str_index+4]
+                        return output
+                    except AttributeError:
+                        try:
+                            str_index = re.search(r"居士", input).start()
+                            output = input[0:str_index+2]    
+                            return output                                    
+                        except:
+                            try:
+                                str_index = re.search(r"神父", input).start()
+                                output = input[0:str_index+2]
+                                return output
+                            except:
+                                try:
+                                    str_index = re.search(r"修女", input).start()
+                                    output = input[0:str_index+2]
+                                    return output
+                                except:
+                                    try:
+                                        str_index = re.search(r"小姐", input).start()
+                                        output = input[0:str_index+2]
+                                        return output
+                                    except:                                    
+                                        output = input
+                                        return output
+
 # setting up database
 conn = sqlite3.connect('sch_contact.db')
 c = conn.cursor()
@@ -27,80 +76,87 @@ for sch_id in id_list:
 
     sch_info = BeautifulSoup(page.content, "html.parser")
 
-    sch_names = sch_info.find_all('dd', class_="xxzl-info-tit")
-    sch_districts = sch_info.find_all('dd', class_="xxzl-info-bh")
-    sch_contacts = sch_info.find_all('dd', class_="xxzl-info-dz c")
-    sch_details = sch_info.find_all('dd', class_="xmcslr01")
+    try:
+        sch_names = sch_info.find_all('dd', class_="xxzl-info-tit")
+        sch_districts = sch_info.find_all('dd', class_="xxzl-info-bh")
+        sch_contacts = sch_info.find_all('dd', class_="xxzl-info-dz c")
+        sch_details = sch_info.find_all('dd', class_="xmcslr01")
 
 
-    for sch_name in sch_names:
-        
-        try:
-            sch_name_full = sch_name.text
-            # print(sch_name_full)
-            sch_name_sep = sch_name_full.splitlines()
-            # print(sch_name_sep)
-            chi_name = re.sub(r'\t', '', sch_name_sep[1])
-            eng_name = re.sub(r'\t', '', sch_name_sep[2])
-        except:
-            break
-    
-    for sch_district in sch_districts:
-        try:
-            district_full = sch_district.text
-            district_full_split = district_full.split('\xa0\xa0\xa0')
-            district = re.sub(r'\n', '', district_full_split[0])
-            #print(district)
-        except:
-            break
-
-    for contact in sch_contacts:
-
-        try:
-            table_1 = contact.find_all('td') 
+        for sch_name in sch_names:
             
-            address = table_1[1].text
-            phone = table_1[3].text
-            email = table_1[6].text
-            fax = table_1[8].text
-            website = table_1[11].text
-
-            # print(address)
-            # print(phone)
-            # print(email)
-            # print(fax)
-            # print(website)
-        except:
-            break    
-
-    for detail in sch_details:
+            try:
+                sch_name_full = sch_name.text
+                # print(sch_name_full)
+                sch_name_sep = sch_name_full.splitlines()
+                # print(sch_name_sep)
+                chi_name = re.sub(r'\t', '', sch_name_sep[1])
+                eng_name = re.sub(r'\t', '', sch_name_sep[2])
+            except:
+                break
         
-        try:
-            table_2 = detail.find_all('td') 
-        
-            supervisor = table_2[2].text
-            principal = re.sub(r"[^\w\s]", '', table_2[5].text)
-            sch_type = '小學'
-            sch_sector = re.sub(r"[^\w\s]", '', table_2[11].text)
-            sch_gender = table_2[14].text
-            religion = table_2[20].text
+        for sch_district in sch_districts:
+            try:
+                district_full = sch_district.text
+                district_full_split = district_full.split('\xa0\xa0\xa0')
+                district = re.sub(r'\n', '', district_full_split[0])
+                #print(district)
+            except:
+                break
 
-            # print(supervisor)
-            # print(principal)
-            # print(sch_type)
-            # print(sch_gender)
-            # print(religion)
-        except:
-            break    
-    
-    # Insert new record
-    if all(v is not None for v in [chi_name, eng_name, district, address, phone, email, fax, website, supervisor, principal, sch_type, sch_sector, sch_gender, religion,record_url]):
-        c.execute('''INSERT INTO school VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (chi_name, eng_name, district, address, phone, email, fax, website, supervisor, principal, sch_type, sch_sector, sch_gender, religion,record_url))
+        for contact in sch_contacts:
+
+            try:
+                table_1 = contact.find_all('td') 
+                
+                address = table_1[1].text
+                phone = table_1[3].text
+                email = table_1[6].text
+                fax = table_1[8].text
+                website = table_1[11].text
+
+                # print(address)
+                # print(phone)
+                # print(email)
+                # print(fax)
+                # print(website)
+            except:
+                break    
+
+        for detail in sch_details:
+            
+            try:
+                table_2 = detail.find_all('td') 
+            
+                supervisor = table_2[2].text
+                supervisor = name_str(supervisor)
+                principal = re.sub(r"[^\w\s]", '', table_2[5].text)
+                principal = name_str(principal)
+                sch_type = '小學'
+                sch_sector = re.sub(r"[^\w\s]", '', table_2[11].text)
+                sch_gender = table_2[14].text
+                religion = table_2[20].text
+
+                # print(supervisor)
+                # print(principal)
+                # print(sch_type)
+                # print(sch_gender)
+                # print(religion)
+            except:
+                break    
+        
+        # Insert new record
+        if all(v is not None for v in [chi_name, eng_name, district, address, phone, email, fax, website, supervisor, principal, sch_type, sch_sector, sch_gender, religion,record_url]):
+            c.execute('''INSERT INTO school VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (chi_name, eng_name, district, address, phone, email, fax, website, supervisor, principal, sch_type, sch_sector, sch_gender, religion,record_url))
+    except:
+        c.execute('''INSERT INTO school VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', ('', '', '', '', '', '', '', '', '', '', '中學', '', '', '',record_url))
+        print('Empty record')
+        continue
 conn.commit()
 
 ## SECONDARY SCHOOL
-id_list = list(range(1, 600))
-id_list.append(7772)
+id_list = list(range(2, 500))
+# id_list.append(7772)
 
 for sch_id in id_list:
 
@@ -154,29 +210,17 @@ for sch_id in id_list:
             try:
                 table_2 = detail.find_all('td') 
                 district = re.sub(r"[^\w]+", '', table_2[2].text)
-                supervisor = re.sub(r"[^\w]+", '', table_2[7].text)
-                principal = re.sub(r"[^\w]+", '', table_2[10].text)
-                if re.search("先生", principal).start() != None:
-                    principal_index = re.search("先生", principal).start()
-                    principal = principal[0:principal_index+2]
-                elif re.search("女士", principal).start() != None:
-                    principal_index = re.search("女士", principal).start()
-                    principal = principal[0:principal_index+2]
-                elif re.search("博士", principal).start() != None:
-                    principal_index = re.search("博士", principal).start()
-                    principal = principal[0:principal_index+2]
-                elif re.search("校長", principal).start() != None:
-                    principal_index = re.search("校長", principal).start()
-                    principal = principal[0:principal_index+2]
-                elif re.search("太平紳士", principal).start() != None:
-                    principal_index = re.search("太平紳士", principal).start()
-                    principal = principal[0:principal_index+5]
-
+                # supervisor = table_2[8].text
+                supervisor = re.sub(r"[^\w]+", '', table_2[8].text)
+                supervisor = name_str(supervisor)
+                # principal = table_2[11].text
+                principal = re.sub(r"[^\w]+", '', table_2[11].text)
+                principal = name_str(principal)
 
                 sch_type = '中學'
-                sch_sector = re.sub(r"[^\w]+", '', table_2[13].text)
-                sch_gender = re.sub(r"[^\w]+", '', table_2[16].text)
-                religion = re.sub(r"[^\w]+", '', table_2[28].text)
+                sch_sector = re.sub(r"[^\w]+", '', table_2[14].text)
+                sch_gender = re.sub(r"[^\w]+", '', table_2[17].text)
+                religion = re.sub(r"[^\w]+", '', table_2[29].text)
 
                 # print(supervisor)
                 # print(principal)
@@ -189,7 +233,6 @@ for sch_id in id_list:
         # Insert new record
         if all(v is not None for v in [chi_name, eng_name, district, address, phone, email, fax, website, supervisor, principal, sch_type, sch_sector, sch_gender, religion,record_url]):
             c.execute('''INSERT INTO school VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (chi_name, eng_name, district, address, phone, email, fax, website, supervisor, principal, sch_type, sch_sector, sch_gender, religion,record_url))
-    
     except:
         c.execute('''INSERT INTO school VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', ('', '', '', '', '', '', '', '', '', '', '中學', '', '', '',record_url))
         print('Empty record')
@@ -225,7 +268,9 @@ for sch_id in id_list:
                 fax = re.sub(r'\t', '', table_1[15].text)
                 website = re.sub(r'\t', '', table_1[19].text)
                 supervisor = re.sub(r"[^\w]+", '', table_1[23].text)
+                supervisor = name_str(supervisor)
                 principal = re.sub(r"[^\w]+", '', table_1[25].text)
+                principal = name_str(principal)                
                 sch_type = '特殊學校 ('+ table_1[11].text+')'
                 sch_sector = re.sub(r"[^\w]+", '', table_1[27].text)
                 sch_gender = re.sub(r"[^\w]+", '', table_1[29].text)
@@ -234,12 +279,11 @@ for sch_id in id_list:
                 # Insert new record
                 if all(v is not None for v in [chi_name, eng_name, district, address, phone, email, fax, website, supervisor, principal, sch_type, sch_sector, sch_gender, religion,record_url]):
                     c.execute('''INSERT INTO school VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', (chi_name, eng_name, district, address, phone, email, fax, website, supervisor, principal, sch_type, sch_sector, sch_gender, religion,record_url))
-
+                    del chi_name, eng_name, district, address, phone, email, fax, website, supervisor, principal, sch_type, sch_sector, sch_gender, religion,record_url 
             except:
                 print('Empty record')
                 c.execute('''INSERT INTO school VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)''', ('', '', '', '', '', '', '', '', '', '', '中學', '', '', '',record_url))
                 continue
-
 
     except:
         print('Empty record')
